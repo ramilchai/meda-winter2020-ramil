1e795fbd51e0ba6a45ce5c6c9d204fab88a62d43
Ramil <ramil.ming@gmail.com>
2020-11-17 11:25:37 -0800

    Localhost for the first time

---
+++ assignments/week_4/express_server/index.js
@@ -0,0 +1,21 @@
+//Bring in the express package.
+const express = require("express");
+
+//Run a copy of the express package.
+const app = express();
+
+//Connects our HTTP server with the Express web module. 
+const http = require("http").Server(app);
+
+//Decide on the port number to listen on when running Express server.
+//Do not use 80, 443, 20, 21, 22, etc.
+const port = 3000;
+
+
+//Tell http module that we will be listening on the number in the port variable.
+http.listen(port);
+
+console.log("Express server is now running on " + port);
+
+//Tell express to load the file public_html folder when someone requests / or nothing.
+app.use("/", express.static("public_html/")); 
