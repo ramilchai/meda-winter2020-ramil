a5a2cbf1f15edb02f5acf957eb72161154873e3e
Ramil <ramil.ming@gmail.com>
2020-11-19 11:40:36 -0800

    request and response between BackEnd and FrontEnd

--- assignments/week_4/express_server/index.js
+++ assignments/week_4/express_server/index.js
@@ -1,6 +1,8 @@
 //Bring in the express package.
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 //Run a copy of the express package.
 const app = express();
 
@@ -15,6 +17,10 @@ const port = 3000;
 //Tell http module that we will be listening on the number in the port variable.
 http.listen(port);
 
+//Body parser so we can automatically convert request objects.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 console.log("Express server is now running on " + port);
 
 //Tell express to load the file public_html folder when someone requests / or nothing.
@@ -24,7 +30,28 @@ app.use("/", express.static("public_html/"));
 app.post("/sayHello", (request, response) => {
     console.log("Someone said hello!");
 
-    let responseObject = {};
+    let winningNumber = Math.floor((Math.random() * 10) + 1);
+
+    console.log(winningNumber);
+
+
+    let dataFromFront = request.body;
+
+    let userNumberChoice = parseInt(dataFromFront.number);
+    let userWinner = false;
+    let outOfRange = false;
+
+    if (winningNumber === userNumberChoice) {
+        userWinner = true;
+    }
+
+    if (userNumberChoice <= 0 || userNumberChoice >= 11) {
+        outOfRange = true;
+    }
+    let responseObject = {
+        results : userWinner,
+        error   : outOfRange
+    };
 
     response.send(responseObject);
 });
