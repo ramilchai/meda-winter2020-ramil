c7154ad2989213740e57b9c94bd640a7d989a30f
Ramil <ramil.ming@gmail.com>
2020-12-08 11:50:03 -0800

    update credentials.js adding front-end file and linking it with back-end

--- assignments/week_7/todo/client/scripts/main.js
+++ assignments/week_7/todo/client/scripts/main.js
@@ -1,3 +1,30 @@
+const base_url = "http://localhost:3000/";
+
 $(document).ready(() => {
-    console.log("Heelo, it's me!")
+    
+    $("#submit").click(() => {
+
+        let priorityNumber = $("#note-priority option:selected").attr("value");
+
+        priorityNumber = parseInt(priorityNumber);
+
+        let noteObject = {
+            auther: null,
+            title: $("#note-title").val(),
+            text: $("#note-text").val(),
+            completed: false,
+            archived: false,
+            priority: priorityNumber,
+            categories: null,
+            timestamp: Date.now()
+        }
+
+        $.post(base_url + "newNote", noteObject, (data) => {
+            console.log(data.saved);
+        });
+
+    });
+
+
+
 });
--- assignments/week_7/todo/index.js
+++ assignments/week_7/todo/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const credentials = require("./credentials.js")
 
 // setting up the Database
-const dbCredentials = "mongodb+srv://todoUser:todoUser@cluster0.wj7tj.mongodb.net/todoApp?retryWrites=true&w=majority";
+const dbCredentials = credentials.dbURL;
 const dbOptions = {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true};
 let dbConnection = mongoose.connect(dbCredentials, dbOptions, (error) => {
     if (error) {
@@ -26,3 +27,15 @@ console.log("Express server is running on port " + port);
 
 // create routes
 app.use("/", express.static("client/"));
+
+app.post("/newNote", (req, res) => {
+    const note = req.body;
+    
+    console.log(note);
+
+    const resObject = {
+        saved: true,
+        error: null
+    }
+    res.send(resObject);
+});
