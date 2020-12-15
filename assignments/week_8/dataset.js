8d8ff58c7d832a4e1e8072049edc2f395ccd7fb2
Ramil <ramil.ming@gmail.com>
2020-12-15 12:26:53 -0800

    Start answering questions

--- assignments/week_8/dataset.js
+++ assignments/week_8/dataset.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const helpers = require("./helpers.js");
+const Title = require("./Title.js");
 
 const data = fs.readFileSync("hourly_rate.csv", "utf-8");
 
@@ -6,26 +8,49 @@ const dataArray = data.split("\n");
 
 dataArray.shift();
 dataArray.pop();
+let titleArray = [];
 
-class Title {
-    constructor(year, title, unionCode, setID, min, max) {
-        this.year = year;
-        this.setID = setID; 
-        this.title = title;
-        this.unionCode = unionCode;
-        this.biWeeklyMin = min;
-        this.biWeeklyMax = max;
-        this.biWeeklyAvg = (min + max) / 2;
-        this.steps = [];
+for (let i = 0; i < dataArray.length; i++) {
+    let entry = dataArray[i].split(",");
+    let fixedEntry = helpers.checkTitleCommas(entry);
+    let entryTitleObject = new Title(fixedEntry[1], fixedEntry[2], fixedEntry[3], fixedEntry[4], fixedEntry[5], parseInt(fixedEntry[7]), parseInt(fixedEntry[8]));
+
+    titleArray.push(entryTitleObject);
+
+}
+
+// let example = dataArray[0].split(",");
+// console.log(example);
+
+// example = helpers.checkTitleCommas(example);
+
+// console.log(example);
+// //question 1 start
+console.log("What Union has the most members?");
+
+let unions = {};
+
+titleArray.forEach(function (element) {
+    let currentUnion = element.unionCode.toString();
+
+    if(unions[currentUnion] === undefined) {
+        unions[currentUnion] = 1; 
+    } else {
+        unions[currentUnion] += 1;
+    }
+});
+
+let highestUnionMemberCount = 0;
+let highestUnionCode = null;
+
+for (let i in unions) {
+    if(unions[i] > highestUnionMemberCount) {
+        highestUnionMemberCount = unions[i];
+        highestUnionCode = i;
     }
 }
 
-const example = dataArray[0].split(",");
-
-console.log(example);
-
-//question 1 start
-
-console.log("What union has the most members?");
-
+console.log(`The union with code ${highestUnionCode} has a member count of ${highestUnionMemberCount}.`)
 //question 1 end
+
+
---
+++ assignments/week_8/helpers.js
@@ -0,0 +1,48 @@
+function checkTitleCommas(titleArray) {
+    if (titleArray[3].charAt(0) === '"') {
+
+        let doubleQFound = false;
+        let currentArrayElement = 3;
+
+        while (!doubleQFound) {
+            let lastCharIndex = titleArray[currentArrayElement].length - 1;
+
+            if (titleArray[currentArrayElement].charAt(lastCharIndex) === '"') {
+                doubleQFound = true;
+            } else {
+                currentArrayElement += 1;
+            }
+        }
+
+        let cutOutArray = titleArray.splice(3, currentArrayElement - 2);
+
+        let combinedTitleString = cutOutArray.join(",");
+
+        titleArray.splice(2, 0, combinedTitleString);
+        
+       
+        // let lastCharacterIndex = titleArray[4].length - 1;
+
+        // if (titleArray[4].charAt(lastCharacterIndex) === '"') {
+
+        //     titleArray[3] = titleArray[3] + titleArray[4];
+
+        //     titleArray.splice(4,1);
+        // } else if (titleArray[5].charAt(titleArray[5].length - 1) === '"') {
+
+        //     titleArray[3] = titleArray[3] + titleArray[4] + titleArray[5];
+
+        //     titleArray.splice(4,2);
+        // }
+
+        return titleArray;
+
+    } else {
+
+        return titleArray;
+    }
+}
+
+module.exports = {
+    checkTitleCommas
+}
